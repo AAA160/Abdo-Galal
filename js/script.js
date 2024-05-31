b651d1033923e328a9f8e6ce711bb9cd798751ed
AAA160 <abdulrahmanawad160@gmail.com>
2024-05-31 21:17:17 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,9 @@
-let count = document.getElementById("count");
-
-function updateCount() {
-  count.innerHTML = parseInt(document.getElementById("num").value) || "...";
-}
-
-function update() {
-  updateCount();
-  if (parseInt(count.innerHTML) === 9000) {
-    addCards(`مبرووووك إجابة صحيحة`);
-  } else if (parseInt(count.innerHTML) > 9000) {
-    addCards(`لا يعم هو مش خول للدرجادي هو اتناك 9000 مرة بس`);
-  } else {
-    addCards(`غلط ،أبو جلال اتناك 9000 مرة`);
-  }
-}
+document.getElementById("how-button").onclick = function () {
+  naho = document.querySelector("video");
+  naho.hidden = false;
+  naho.play();
+  console.log("زبي");
+};
 
 function addCards(...values) {
   const container = document.getElementById("container");
